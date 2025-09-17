1815dc09eb635e21b217c7cd01afd23bb1d62ccd
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-17 13:53:02 -0400

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,11 +1,11 @@
 # Mapping for competitors_history to sub-category CSVs
 COMPETITOR_TO_SUBCATEGORY_MAP = {
-    "competitors_history - BL.csv": "sub_BL.csv",
-    "competitors_history - GS.csv": "sub_GS.csv",
-    "competitors_history - IC.csv": "sub_IC.csv",
-    "competitors_history - LGM.csv": "sub_LGM.csv",
-    "competitors_history - QC.csv": "sub_QC.csv",
-    "competitors_history - RIO.csv": "sub_RIO.csv",
-    "competitors_history - UR.csv": "sub_UR.csv",
-   "synthethic3.csv": "sub_SYN.csv"
+    "competitors_history - BL.csv": "sub-categories/sub_BL.csv",
+    "competitors_history - GS.csv": "sub-categories/sub_GS.csv",
+    "competitors_history - IC.csv": "sub-categories/sub_IC.csv",
+    "competitors_history - LGM.csv": "sub-categories/sub_LGM.csv",
+    "competitors_history - QC.csv": "sub-categories/sub_QC.csv",
+    "competitors_history - RIO.csv": "sub-categories/sub_RIO.csv",
+    "competitors_history - UR.csv": "sub-categories/sub_UR.csv",
+   "synthethic3.csv": "sub-categories/sub_SYN.csv"
 }
