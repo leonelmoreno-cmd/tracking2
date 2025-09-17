3b87e06bd6114059faf722cfd8b8c39abab325f8
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-17 08:13:20 -0400

    Create mapping.py

---
+++ mapping.py
@@ -0,0 +1,32 @@
+# mapping.py (script aparte)
+# Diccionario que vincula cada CSV principal con su CSV de best sellers
+csv_mapping = {
+    "BL": {
+        "history": "competitors_history - BL.csv",
+        "sub": "sub_BL.csv"
+    },
+    "GS": {
+        "history": "competitors_history - GS.csv",
+        "sub": "sub_GS.csv"
+    },
+    "IC": {
+        "history": "competitors_history - IC.csv",
+        "sub": "sub_IC.csv"
+    },
+    "LGM": {
+        "history": "competitors_history - LGM.csv",
+        "sub": "sub_LGM.csv"
+    },
+    "QC": {
+        "history": "competitors_history - QC.csv",
+        "sub": "sub_QC.csv"
+    },
+    "RIO": {
+        "history": "competitors_history - RIO.csv",
+        "sub": "sub_RIO.csv"
+    },
+    "UR": {
+        "history": "competitors_history - UR.csv",
+        "sub": "sub_UR.csv"
+    }
+}
