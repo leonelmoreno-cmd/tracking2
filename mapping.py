894a7f954ebfc5c22f01c855b89b1fc7694bdbd3
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-17 08:53:54 -0400

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -29,7 +29,7 @@ csv_mapping = {
         "history": "competitors_history - UR.csv",
         "sub": "sub_UR.csv"
     },
-    "Synthetic": {
+    "synthethic3.csv": {
         "history": "synthethic3.csv",
         "sub": "sub_SYN.csv"
     }
