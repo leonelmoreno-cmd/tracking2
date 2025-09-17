5f4abba2fe474d0698647292adbe13b42950e383
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-17 13:19:44 -0400

    Create mapping.py

---
+++ mapping.py
@@ -0,0 +1,11 @@
+# Mapping for competitors_history to sub-category CSVs
+COMPETITOR_TO_SUBCATEGORY_MAP = {
+    "competitors_history - BL.csv": "sub_BL.csv",
+    "competitors_history - GS.csv": "sub_GS.csv",
+    "competitors_history - IC.csv": "sub_IC.csv",
+    "competitors_history - LGM.csv": "sub_LGM.csv",
+    "competitors_history - QC.csv": "sub_QC.csv",
+    "competitors_history - RIO.csv": "sub_RIO.csv",
+    "competitors_history - UR.csv": "sub_UR.csv",
+   "synthethic3.csv": "sub_SYN.csv"
+}
