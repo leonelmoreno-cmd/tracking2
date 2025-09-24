d4f812698e95e53c5df96f36d7df9aef7a4cda2e
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-24 07:42:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ from pages.summary import main as summary_page
 from pages.best_sellers import main as best_sellers_page
 from pages.detailed_table import main as detailed_table_page
 from pages.settings import main as settings_page
-from pages.traffic import main as traffic_page   # <-- NEW
-
+from pages.traffic import main as traffic_page   
+from pages.evolution import main as evolution
 def main():
     st.set_page_config(page_title="Competitor Price Monitoring", page_icon="📊")
 
     pages = [
         st.Page(home_page, title="Overview", icon="🏠", default=True),
         st.Page(summary_page, title="Breakdown by ASIN", icon="📈", url_path="summary"),
+         st.Page(evolution, title="Evolution", icon="🔍", url_path="evolution"),
         st.Page(best_sellers_page, title="Best Sellers Rank", icon="⭐", url_path="best-sellers"),
         st.Page(detailed_table_page, title="Detailed Table", icon="📋", url_path="detailed-table"),
-        st.Page(traffic_page, title="Web Traffic", icon="🌐", url_path="traffic"),  # <-- NEW
+        st.Page(traffic_page, title="Web Traffic", icon="🌐", url_path="traffic"),
         st.Page(settings_page, title="Settings", icon="⚙️", url_path="settings"),
     ]
 
