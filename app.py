8c11182431f0e74afb9fd718039a551a9d7a611a
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-24 07:58:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from pages.detailed_table import main as detailed_table_page
 from pages.settings import main as settings_page
 from pages.traffic import main as traffic_page   
 from pages.evolution import main as evolution
+
 def main():
     st.set_page_config(page_title="Competitor Price Monitoring", page_icon="📊")
 
@@ -14,6 +15,7 @@ def main():
         st.Page(home_page, title="Overview", icon="📊", default=True),
         st.Page(summary_page, title="Breakdown by ASIN", icon="📈", url_path="summary"),
         st.Page(evolution, title="Evolution", icon="🔍", url_path="evolution"),
+        st.Page(sales, title="Sales Estimate", icon="💰", url_path="sales")
         st.Page(best_sellers_page, title="Best Sellers Rank", icon="⭐", url_path="best-sellers"),
         st.Page(detailed_table_page, title="Detailed Table", icon="📋", url_path="detailed-table"),
         st.Page(traffic_page, title="Web Traffic", icon="🌐", url_path="traffic"),
