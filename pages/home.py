aa6e9d4483f8901b12281a076f1ff69f0f01fa00
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-25 11:27:35 -0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -56,6 +56,4 @@ def main():
         df_overview, brands_to_plot=None, period=period
     )
     st.plotly_chart(overview_fig, use_container_width=True)
-    st.write(prepared_df.columns)
-    st.dataframe(prepared_df[["asin", "brand", "product_title", "product_photo"]].head())
     render_current_basket_gallery(prepared_df, columns=5)
