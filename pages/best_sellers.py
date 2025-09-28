3cefbd757593b4b59760932569e9812b565dddd3
leonelmoreno-cmd <leonel.jungleclick@gmail.com>
2025-09-28 18:52:33 -0400

    prueba leonel
    Esto es un tem

--- pages/best_sellers.py
+++ pages/best_sellers.py
@@ -10,7 +10,7 @@ def main():
 
     DEFAULT_BASKET = "synthethic3.csv"
     active_basket_name, active_url, name_to_url = resolve_active_basket(DEFAULT_BASKET)
-
+    #Prueba
     # Optionally allow the users to toggle baskets
     period, active_basket_name = render_basket_and_toggle(name_to_url, active_basket_name, DEFAULT_BASKET)
     active_url = name_to_url.get(active_basket_name, active_url)
